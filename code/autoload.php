722e66a5ae005f821ee18f8d864ca475aba369f0
Marcello Costa <marcello88costa@yahoo.com.br>
2021-03-26 12:18:33 -0300

    Simplificando path dos módulos

--- code/autoload.php
+++ code/autoload.php
@@ -2,11 +2,12 @@
 
 $composer = require __DIR__ . '/vendor/autoload.php';
 
-$modules = path_modules('/src/');
+$modules = path_modules();
+
 foreach ($modules as &$module){
-  $namespace = explode ('/', $module);
-  $namespace = $namespace[count($namespace)-2];
-  $composer->setPsr4('SlimExample\\'.$namespace.'\\', $module);
+  $moduleName = substr($module, strpos($module, 'Modules')+8);
+  $modulePath = substr($module, strpos($module, 'src'));
+  $composer->setPsr4('SlimExample\\Modules\\'.$moduleName."\\", $module);
 }
 
 return $composer;
--- code/helpers/path_loader.php
+++ code/helpers/path_loader.php
@@ -2,7 +2,7 @@
 
 if (!function_exists('path_modules')){
   function path_modules($suffix = ''){
-    $paths = glob(__DIR__.'../../src/Modules/*', GLOB_ONLYDIR);
+    $paths = glob(__DIR__.'/../src/Modules/*', GLOB_ONLYDIR);
 
     if ($suffix === ""){
       return $paths;
