1f2581712ceff421552893fe5c2f4ba8b41b62fc
Marcello Costa <marcello88costa@yahoo.com.br>
2021-03-26 12:18:54 -0300

    Adicionando console

---
+++ code/src/Modules/Core/Commands/ServerCommand.php
@@ -0,0 +1,17 @@
+<?php
+namespace SlimExample\Modules\Core\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ServerCommand extends Command {
+  protected function configure(){
+    $this->setName('server')->setDescription('run a PHP Built-in Server');
+  }
+  protected function execute(InputInterface $input, OutputInterface $output){
+    $output->write('Run server in http://localhost:8080');
+
+    exec('php -S localhost:8080 -t ' . getcwd() . DIRECTORY_SEPARATOR . "public");
+  }
+}
