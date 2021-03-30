7397990f20c1b1e2c0c126701b16fb630fcd20ab
Marcello Costa <marcello88costa@yahoo.com.br>
2021-03-30 15:27:23 -0300

    Adicionando autoload de libs

--- code/autoload.php
+++ code/autoload.php
@@ -3,11 +3,17 @@
 $composer = require __DIR__ . '/vendor/autoload.php';
 
 $modules = path_modules();
+$libs = path_libs();
 
 foreach ($modules as &$module){
   $moduleName = substr($module, strpos($module, 'Modules')+8);
-  $modulePath = substr($module, strpos($module, 'src'));
   $composer->setPsr4('SlimExample\\Modules\\'.$moduleName."\\", $module);
+}
+
+
+foreach ($libs as &$lib){
+  $libName = substr($lib, strpos($lib, 'Lib')+4);
+  $composer->setPsr4('SlimExample\\Lib\\'.$libName."\\", $lib);
 }
 
 return $composer;
--- code/helpers/path_loader.php
+++ code/helpers/path_loader.php
@@ -15,3 +15,19 @@ if (!function_exists('path_modules')){
     return $paths;
   }
 }
+
+if (!function_exists('path_libs')){
+  function path_libs($suffix = ''){
+    $paths = glob(__DIR__.'/../src/Lib/*', GLOB_ONLYDIR);
+
+    if ($suffix === ""){
+      return $paths;
+    }
+
+    foreach($paths as &$path){
+      $path = $path . $suffix;
+    }
+
+    return $paths;
+  }
+}
