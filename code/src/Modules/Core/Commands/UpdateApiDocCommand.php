c2c8f605fb934c21288faafec3c321d7574788bd
Marcello Costa <marcello88costa@yahoo.com.br>
2021-03-30 19:10:27 -0300

    Adicionando comando de update da documentação da API

---
+++ code/src/Modules/Core/Commands/UpdateApiDocCommand.php
@@ -0,0 +1,22 @@
+<?php
+namespace SlimExample\Modules\Core\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class UpdateApiDocCommand extends Command {
+  protected function configure(){
+    $this
+    ->setName('updateapidoc')
+    ->setDescription('Update the API documentation file');
+  }
+
+  protected function execute(InputInterface $input, OutputInterface $output): int {
+    exec('vendor/bin/openapi -o openapi.json src');
+    
+    echo PHP_EOL."Documentation updated".PHP_EOL.PHP_EOL;
+    return Command::SUCCESS;
+  }
+}
