42ea31cfc55ab13682b31a2b46739342c3022840
Marcello Costa <marcello88costa@yahoo.com.br>
2021-04-04 12:17:10 -0300

    Melhorando #TODO do Users controller

--- code/src/Modules/Users/Controllers/UsersController.php
+++ code/src/Modules/Users/Controllers/UsersController.php
@@ -9,24 +9,27 @@ use SlimExample\Modules\Users\Models\User;
 class UsersController extends ControllerAbstract {
     public static function get(Request $request, Response $response, array $args): Response {
         # TODO
-        $users = User::get();
-        return $response->withJson($users);
+        /*$users = User::get();
+        return $response->withJson($users);*/
+        return $response->withJson('#TODO Get Users');
     }
 
     public static function post(Request $request, Response $response, array $args): Response {
         # TODO
-        $data = $request->getParsedBody();
+        /*$data = $request->getParsedBody();
         $user = User::create($data);
-        return $response->withJson($user);
+        return $response->withJson($user);*/
+
+        return $response->withJson('#TODO Post Users');
     }
 
     public static function put(Request $request, Response $response, array $args): Response {
         # TODO
-        return $response->withJson('Put Users');
+        return $response->withJson('#TODO Put Users');
     }
 
     public static function delete(Request $request, Response $response, array $args): Response {
         # TODO
-        return $response->withJson('Delete Users');
+        return $response->withJson('#TODO Delete Users');
     }
 }
